5e9a788616cfd7e1a44c663529ed1ab7a8282df4
guilacerda <guiglacerda@gmail.com>
2019-04-06 23:31:36 -0300

    Changes timeout

--- rasa/actions/calendar.py
+++ rasa/actions/calendar.py
@@ -15,24 +15,14 @@ class ActionCalendar(Action):
         dispatcher.utter_message('Calma aí, rapidinho!')
         dispatcher.utter_message('Vou buscar isso daí para você')
         crawlerRegister = 'https://webcrawler-matricula.botlino.com.br'
-        try:
-            requests.get(
-                f'{crawlerRegister}/registration/downloadPdf',
-                timeout=3
-            )
-            data = {
-                'text': 'Aqui está o calendário de matrícula. '
-                        'Nele você pode adquirir informações de datas sobre: '
-                        'trancamento geral, parcial, período de matrícula, '
-                        'pré-matrícula, ajuste...',
-                'image': f'{crawlerRegister}/registration/downloadPdf'
-            }
-            dispatcher.utter_response(data)
-        except Exception as exception:
-            dispatcher.utter_message(
-                "Tive um problema ao pegar o calendário acadêmico pra você... "
-                "Tenta mais tarde! Para que houve um problema no site onde "
-                "busco..."
-            )
+        requests.get(f'{crawlerRegister}/registration/downloadPdf')
+        data = {
+            'text': 'Aqui está o calendário de matrícula. '
+                    'Nele você pode adquirir informações de datas sobre: '
+                    'trancamento geral, parcial, período de matrícula, '
+                    'pré-matrícula, ajuste...',
+            'image': f'{crawlerRegister}/registration/downloadPdf'
+        }
+        dispatcher.utter_response(data)
 
         return []
