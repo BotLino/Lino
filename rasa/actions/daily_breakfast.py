3d3e896e1b8496e19992e9ef3ef4a0fe0ca33b97
Amanda Bezerra <amandabs_contato@hotmail.com>
2019-06-25 00:26:57 -0300

    Applies clean code techniques in daily_breakfast action - Applies methods composition - Applies explaining methods

--- rasa/actions/daily_breakfast.py
+++ rasa/actions/daily_breakfast.py
@@ -15,20 +15,10 @@ class ActionDailyBreakfast(Action):
         return "action_daily_breakfast"
 
     def run(self, dispatcher, tracker, domain):
-        messages = []
-
-        day = time.strftime('%A', time.localtime())
-
-        tracker_state = tracker.current_state()
-        logging.warning(tracker_state)
-        sender_id = tracker_state['sender_id']
+        day = self.get_current_day()
 
         try:
-            response = requests.get(
-                'http://webcrawler-ru.botlino.com.br/cardapio/{}/desjejum'
-                .format(day),
-                timeout=1
-            ).json()
+            menu = self.request_menu(day)
         except TimeoutError as timeouterror:
             dispatcher.utter_message(
                 "Tentei pegar o cardápio mas minha net não cooperou..."
@@ -43,47 +33,74 @@ class ActionDailyBreakfast(Action):
             )
             return []
 
-        lunch_menu = ""
-
-        for label in response:
-            dish = str(
-                '*' + label + '*' + ': ' +
-                response[label] + '\n')
-            lunch_menu += dish
-
-        messages.append(lunch_menu)
+        menu_messages = self.format_menu(menu)
 
         welcome_message = 'Eai! Então... Pro café, nós teremos: '
 
+        sender_id = self.get_sender_id(tracker)
+
+        data = self.request_user_data(sender_id, welcome_message)
+
+        if not data['ok']:
+            dispatcher.utter_message(welcome_message)
+            self.send_messages_to_facebook(menu_messages)
+        else:
+            self.send_messages_to_telegram(sender_id, menu_messages)
+
+        return []
+
+    def get_current_day(self):
+        return time.strftime('%A', time.localtime())
+
+    def get_sender_id(self, tracker):
+        tracker_state = tracker.current_state()
+        logging.warning(tracker_state)
+        return tracker_state['sender_id']
+
+    def request_menu(self, day):
+        return requests.get(
+            'http://webcrawler-ru.botlino.com.br/cardapio/{}/desjejum'
+            .format(day),
+            timeout=1
+        ).json()
+
+    def format_menu(self, menu):
+        formatted_menu = ""
+        messages = []
+
+        for label in menu:
+            dish = str(
+                '*' + label + '*' + ': ' +
+                menu[label] + '\n')
+            formatted_menu += dish
+
+        messages.append(formatted_menu)
+        return messages
+
+    def request_user_data(self, sender_id, message):
         data = requests.post(
             '{}/bot{}/sendMessage'
             .format(API_URL, ACCESS_TOKEN),
             data={
                 'chat_id': sender_id,
-                'text': welcome_message
+                'text': message
             }
         ).json()
-        messenger = ""
-        # Check user is from Telegram or Facebook
-        if not data['ok']:
-            dispatcher.utter_message(welcome_message)
-            messenger = "Facebook"
-        else:
-            messenger = "Telegram"
 
-        if(messenger == "Telegram"):
-            for message in messages:
-                requests.post(
-                    '{}/bot{}/sendMessage'
-                    .format(API_URL, ACCESS_TOKEN),
-                    data={
-                        'chat_id': sender_id,
-                        'text': message,
-                        'parse_mode': PARSE
-                    }
-                )
-        elif(messenger == "Facebook"):
-            for message in messages:
-                dispatcher.utter_message(message)
+        return data
 
-        return []
+    def send_messages_to_telegram(self, sender_id, messages):
+        for message in messages:
+            requests.post(
+                '{}/bot{}/sendMessage'
+                .format(API_URL, ACCESS_TOKEN),
+                data={
+                    'chat_id': sender_id,
+                    'text': message,
+                    'parse_mode': PARSE
+                }
+            )
+
+    def send_messages_to_facebook(self, messages):
+        for message in messages:
+            dispatcher.utter_message(message)
